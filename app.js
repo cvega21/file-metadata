ad885e3c9028ef836fef30176cf654c8423cf570
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-04-30 02:05:19 -0500

    fixed cors

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+var cors = require('cors');
 
 app.use('/', indexRouter);
 app.use('/api', usersRouter);
